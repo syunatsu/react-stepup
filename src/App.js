64952927fed4e89553dc6166b8651766a1bc63eb
syunatsu <myf.tyys@gmail.com>
2022-06-20 13:09:46 +0900

    react-stepup1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import './App.css';
+import { ChildArea } from './ChildArea';
+
 
 function App() {
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input value={text} onChange={onChangeText}/>
+     <br />
+     <br />
+     <button onClick={onClickOpen}>表示</button>
+     <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
---
+++ src/ChildArea.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { memo } from 'react';
+
+const style = {
+  width: '100%',
+  height: '200px',
+  backgroundColor: 'khaki'
+}
+
+export const ChildArea = memo(({open, onClickClose}) => {
+  
+  console.log('ChildAreaがレンダリングされた！！');
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log('...');
+  });
+
+  return (
+    <>
+    {open ? (
+      <div style={style}>
+        <p>子コンポーネント</p>
+        <button onClick={onClickClose}>閉じる</button>
+      </div>
+      ) : null}  
+    </>
+  )
+});
